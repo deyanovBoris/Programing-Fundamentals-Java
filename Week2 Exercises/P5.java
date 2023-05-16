6d429ac15f8b5351a2d1190e89c8bd14436eb6f7
deyanovBoris <133798115+deyanovBoris@users.noreply.github.com>
2023-05-16 19:58:37 +0300

    Add commented description

--- Week2 Exercises/P5.java
+++ Week2 Exercises/P5.java
@@ -2,6 +2,8 @@ package FundEx2;
 
 import java.util.Scanner;
 
+/* this code prints out the values between the starting and ending characters inputed by user */
+
 public class P5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
