3d6b1bda3b962a576c352661f4188e5acb0f235e
Boris Deyanov <133798115+deyanovBoris@users.noreply.github.com>
2023-05-27 10:58:24 +0300

    Addded problem description and method clarification

--- Week3 Exercises/P8MagicSum.java
+++ Week3 Exercises/P8MagicSum.java
@@ -3,6 +3,10 @@ package FundEx3;
 import java.util.Arrays;
 import java.util.Scanner;
 
+/*
+Write a program that prints all unique pairs in an array of integers whose sum is equal to a given number.
+*/
+
 public class P8MagicSum {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -17,7 +21,7 @@ public class P8MagicSum {
         // Get first element of array to sum up.
         for (int i = 0; i < numbersArray.length; i++) {
             // Sum up the second number starting from the back to the front of the
-            // array.
+            // array. This is to avoid double counting (e.g. 7 and 6 and 6 and 7 being output).
             for (int j = numbersArray.length - 1; j > i; j--) {
                 if (numbersArray[i] + numbersArray[j] == targetValue) {
                     System.out.printf("%d %d%n", numbersArray[i], numbersArray[j]);
