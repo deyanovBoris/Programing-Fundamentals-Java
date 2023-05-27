327f727be3486e010b7f5131c0d62baa05950d75
Boris Deyanov <133798115+deyanovBoris@users.noreply.github.com>
2023-05-27 10:55:02 +0300

    Added problem description

--- Week3 Exercises/P2CommonElements.java
+++ Week3 Exercises/P2CommonElements.java
@@ -2,6 +2,11 @@ package FundEx3;
 
 import java.util.Scanner;
 
+/*
+Write a program that prints common elements in two arrays.
+You have to compare the elements of the second array to the elements of the first.
+*/
+
 public class P2CommonElements {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
