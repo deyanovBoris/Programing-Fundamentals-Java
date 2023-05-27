5dbae0b7cc76f964c765b984e269ffe0b65d24c2
Boris Deyanov <133798115+deyanovBoris@users.noreply.github.com>
2023-05-27 10:51:26 +0300

    Added problem description and comment

--- Week3 Exercises/P5TopIntegers.java
+++ Week3 Exercises/P5TopIntegers.java
@@ -3,10 +3,16 @@ package FundEx3;
 import java.util.Scanner;
 import java.util.Arrays;
 
+/*
+Write a program to find all the top integers in an array. 
+A top integer is an integer that is bigger than all the elements to its right.
+*/
+
 public class P5TopIntegers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        
+        //Input array as a single line of integer numbers separated with a space.
         int[] numbers = Arrays
                 .stream(scanner.nextLine().split(" "))
                 .mapToInt(e -> Integer.parseInt(e)).toArray();
