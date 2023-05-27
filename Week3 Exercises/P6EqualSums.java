57272145f26710bc0f893a916c24f693590d683b
Boris Deyanov <133798115+deyanovBoris@users.noreply.github.com>
2023-05-27 10:52:51 +0300

    Added problem description

--- Week3 Exercises/P6EqualSums.java
+++ Week3 Exercises/P6EqualSums.java
@@ -3,10 +3,19 @@ package FundEx3;
 import java.util.Arrays;
 import java.util.Scanner;
 
+/*
+Write a program that determines if an element exists in the array such that
+the sum of the elements on its left is equal to the sum of the elements on
+its right. If there are no elements to the left/right, their sum is considered
+to be 0.
+Print the index that satisfies the required condition or "no" if there is no such index.
+*/
+
 public class P6EqualSums {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        //Input integers as one line separated by spaces
         int[] numbersArray = Arrays.stream(scanner.nextLine().split(" "))
                 .mapToInt(e -> Integer.parseInt(e))
                 .toArray();
